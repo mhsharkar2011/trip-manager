181f8db9b7a7a5a91153e26d46973ec6070fd64b
Tarif <faisal.tarif@itconquest.com>
2021-03-24 18:59:14 +0600

    Superadmin panel - For the API explorer package, set a user token automatically

--- app/helpers.php
+++ app/helpers.php
@@ -13,3 +13,14 @@ function is_non_prod() {
 function get_super_admin_email() {
     return User::getSuperAdmin('email');
 }
+
+function __getTokenForPostman() {
+    $user = User::superAdmin()->first();
+    $token = $user->tokens()->whereName('postman')->value('token');
+
+    if (! $token) {
+        $token = $user->createToken('postman')->plainTextToken;
+    }
+
+    return $token;
+}
