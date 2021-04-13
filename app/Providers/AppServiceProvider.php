3e6a8055e6f5e964547d30fdde36721a9cb2103a
Tarif <faisal.tarif@itconquest.com>
2021-04-13 16:39:10 +0600

    Force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('FORCE_HTTPS')) {
+           \URL::forceScheme('https');
+        }
     }
 }
