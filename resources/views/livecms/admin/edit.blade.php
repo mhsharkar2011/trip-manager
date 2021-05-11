435b56aa43be7042ef4de37a557b503a78cb1598
fuad <fuad@itconquest.com>
2021-05-11 20:16:47 +0600

    title error fixed for pages add and edit file.

--- resources/views/livecms/admin/edit.blade.php
+++ resources/views/livecms/admin/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout title="Edit Page">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -65,9 +65,9 @@
                         <br>
 
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                            <x-button class="ml-3">
+                            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                 Save
-                            </x-button>
+                            </button>
                         </div>
                     </form>
 
