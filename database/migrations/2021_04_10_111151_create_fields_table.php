33082e3f3eb54b7dfc641b7038d01f24f3a2a9f8
Tarif <faisal.tarif@itconquest.com>
2021-05-12 00:34:40 +0600

    Devpanel - Wrong ref to model class after refactoring

--- database/migrations/2021_04_10_111151_create_fields_table.php
+++ database/migrations/2021_04_10_111151_create_fields_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Entity;
+use App\Devpanel\Models\Entity;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
