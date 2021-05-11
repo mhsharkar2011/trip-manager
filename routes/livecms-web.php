79f5e126b3eb56c20252ca8d13a1629fff1b5b87
Tarif <faisal.tarif@itconquest.com>
2021-05-11 21:05:16 +0600

    LiveCMS text edit related files added

--- routes/livecms-web.php
+++ routes/livecms-web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\LiveCMS\Models\Template;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 use App\LiveCMS\Controllers\ImagesController;
 use App\LiveCMS\Controllers\PagesController;
 use App\LiveCMS\Controllers\TextEditController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +29,10 @@ Route::post('livecms/text-update', [TextEditController::class, 'update'])
 
 Route::resource('cms', PagesController::class)
 ->middleware('auth:sanctum');
+
+if (Schema::hasTable('templates')) {
+    foreach(Template::all() as $tpl) {
+        $data = [];
+        Route::view($tpl->route, $tpl->path, $data)->name($tpl->path);
+    }
+}
