c1194db977a500a4536c5b21392c504f7a6f91c2
Tarif <faisal.tarif@itconquest.com>
2023-01-05 17:38:43 +0600

    Docker for local setup - updated steps and added few more conveniences

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,11 @@ Route::prefix('v1')->group(function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware([
+    'auth:sanctum'
+])
+->group(function () {
+    //auth required routes will go here
+});
