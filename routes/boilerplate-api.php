cbf3ff1dc63692f6121e94ea3d40e40d3db950f1
Junaet Hossain <junaet.hossain@itconquest.com>
2023-02-03 10:56:47 +0600

    add route prefix for boilerplate api.

--- routes/boilerplate-api.php
+++ routes/boilerplate-api.php
@@ -21,24 +21,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('entities', EntityController::class);
-Route::apiResource('entities.fields', EntityFieldController::class);
+Route::prefix('v1')->group(function () { 
 
-Route::post('entities/{entity}/crud-generate', CrudGenerateController::class);
-
-Route::resource('tenants', TenantController::class, ['except' => ['create', 'edit']]);
-
-/**
- * One endpoint used by all entities for retrieving, uploading attachments
- */
-Route::get('{entity}/{id}/attachments', [AttachmentController::class, 'get'])
-->where('entity', '[a-z-A-Z]+')->whereNumber('id');
-
-Route::post('{entity}/{id}/attachments', [AttachmentController::class, 'upload'])
-->where('entity', '[a-z-A-Z]+')->whereNumber('id');
-
-Route::delete('{entity}/{id}/attachments/{attachment_id}', [AttachmentController::class, 'delete'])
-->where('entity', '[a-z-A-Z]+')->whereNumber('id')->whereNumber('attachment_id');
-
-Route::post('{entity}/{id}/attachments/{attachment_id}/attach', [AttachmentController::class, 'attach'])
-->where('entity', '[a-z-A-Z]+')->whereNumber('id')->whereNumber('attachment_id');
+    Route::apiResource('entities', EntityController::class);
+    Route::apiResource('entities.fields', EntityFieldController::class);
+    
+    Route::post('entities/{entity}/crud-generate', CrudGenerateController::class);
+    
+    Route::resource('tenants', TenantController::class, ['except' => ['create', 'edit']]);
+    
+    /**
+     * One endpoint used by all entities for retrieving, uploading attachments
+     */
+    Route::get('{entity}/{id}/attachments', [AttachmentController::class, 'get'])
+    ->where('entity', '[a-z-A-Z]+')->whereNumber('id');
+    
+    Route::post('{entity}/{id}/attachments', [AttachmentController::class, 'upload'])
+    ->where('entity', '[a-z-A-Z]+')->whereNumber('id');
+    
+    Route::delete('{entity}/{id}/attachments/{attachment_id}', [AttachmentController::class, 'delete'])
+    ->where('entity', '[a-z-A-Z]+')->whereNumber('id')->whereNumber('attachment_id');
+    
+    Route::post('{entity}/{id}/attachments/{attachment_id}/attach', [AttachmentController::class, 'attach'])
+    ->where('entity', '[a-z-A-Z]+')->whereNumber('id')->whereNumber('attachment_id');
+    
+});
