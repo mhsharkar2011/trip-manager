d1b594884eb2c06567bf60a3d6a248f8d8c9a5a5
Tarif <faisal.tarif@itconquest.com>
2023-02-06 13:15:12 +0600

    Migration error - Spatie permission table is not getting added to migrations table, so throwing error everytime migrate is run, for now adding a hotfix

--- database/migrations/2023_02_02_094012_create_permission_tables.php
+++ database/migrations/2023_02_02_094012_create_permission_tables.php
@@ -25,7 +25,8 @@ class CreatePermissionTables extends Migration
             throw new \Exception('Error: team_foreign_key on config/permission.php not loaded. Run [php artisan config:clear] and try again.');
         }
 
-        Schema::create($tableNames['permissions'], function (Blueprint $table) {
+        // Schema::create($tableNames['permissions'], function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->bigIncrements('id'); // permission id
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
