4733f62e6d30740abc294123b93a2d96997d3852
Tarif <faisal.tarif@itconquest.com>
2023-02-15 13:35:18 +0600

    Event mechanism - Label changes for the existing consume command for consistency, keeping the previous one for now

---
+++ app/Console/Commands/RabbitMQConsume.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\RabbitMQService;
+use Illuminate\Console\Command;
+
+
+class RabbitMQConsume extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'project:rabbitmq-consume 
+    {--R|routing-key= : Routing key for the queue, for routing key formats check "Topic exchange" seciton under https://www.rabbitmq.com/tutorials/tutorial-five-php.html}
+    ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $routing_key = $this->option('routing-key');
+
+        if (! $routing_key) {
+            $routing_key = $this->ask('
+                Please enter the routing key for the queue. Our usual format is <app>.<entity>.<action>, so some examples for routing keys,
+                # - listens for all events
+                pmapp.project.* - listens for all pmapp project related events
+                pmapp.# - listens for all pmapp related events
+            ');
+        }
+
+        $callback = function ($msg) {
+            echo 'received event from broker' . PHP_EOL;
+            print_r(json_decode($msg->body, true));
+
+            logger('received event from broker: ' . $msg->delivery_info['routing_key'], (array) $msg->body);
+            $msg->ack();
+        };
+
+
+        RabbitMQService::consume(
+            $routing_key,
+            $callback
+        );
+        
+        return 0;
+    }
+}
