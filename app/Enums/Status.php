2841c4df5077f7ed8cdb83154bc046ae6a8c7089
monirbd <monirbd.me@gmail.com>
2023-02-22 10:31:31 +0600

    Enum Status add.ed

---
+++ app/Enums/Status.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+class Status
+{
+    const DRAFT = "DRAFT";
+    const PUBLISHED = "PUBLISHED";
+    const UNPUBLISHED = "UNPUBLISHED";
+    const ACTIVE = "ACTIVE";
+    const INACTIVE = "INACTIVE";
+    const REJECTED = "REJECTED";
+    const ARCHIVED = "ARCHIVED";
+}
