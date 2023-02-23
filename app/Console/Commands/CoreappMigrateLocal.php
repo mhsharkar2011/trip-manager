184dd4958f8ff0a59bb330e54bc0fc602d4d8c94
Tarif <faisal.tarif@itconquest.com>
2023-02-23 19:11:06 +0600

    Coreapp compatibility - Added another command to make necessary adjustments to server coreapp database

--- app/Console/Commands/CoreappMigrateLocal.php
+++ app/Console/Commands/CoreappMigrateLocal.php
@@ -5,21 +5,21 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class CoreappMigrate extends Command
+class CoreappMigrateLocal extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'project:coreapp-migrate';
+    protected $signature = 'project:coreapp-migrate-local';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Run this on a server that is connected to coreapp database, this will make adjustments to it for boilerplate compatibility';
+    protected $description = 'Run this to prepare an empty database so that it works with coreapp data';
 
     /**
      * Create a new command instance.
@@ -38,27 +38,33 @@ class CoreappMigrate extends Command
      */
     public function handle()
     {
-        $this->comment('Inserting rows in migrations table that were created manually');
+        //server
+        //enter projects in migrations
+        //delete table: roles, permissions
+        //add tenant_id int(10) nullable in projects table
+        //UPDATE `projects` set tenant_id=company_id;
+
+
+        $this->comment('Importing the following sql file');
+        $this->line(database_path('/coreapp-dev-schema.sql'));
+        
+        DB::unprepared(file_get_contents(database_path('/coreapp-dev-schema.sql')));
+        
+        $this->comment('Imported coreapp tables');
+
+        //insert migration files in migration tables that has already been created manually
         DB::table('migrations')->insert([
             ['migration' => '2023_01_24_095644_create_projects_table', 'batch' => 1]
+            // ,['migration' => '2023_02_02_094012_create_permission_tables', 'batch' => 1]
         ]);
-        
-        $this->comment('Dropping conflicting tables');
-        DB::statement('DROP TABLE IF EXISTS `roles`'); //seems to be unused and conflicts with boilerplate role permission tables
-        DB::statement('DROP TABLE IF EXISTS `permissions`'); //seems to be unused and conflicts with boilerplate role permission tables
 
-        $this->comment('Running boilerplate migrate now');
+        DB::statement('DROP TABLE `roles`'); //seems to be unused and conflicts with boilerplate role permission tables
+
+        $this->comment('Running artisan migrate now');
         $this->call('migrate');
 
-        $this->comment('Adding tenant_id to projects table');
-        DB::statement('ALTER TABLE `projects` ADD `tenant_id` INT(10) NULL DEFAULT NULL AFTER `avatar_directory`'); 
-        DB::statement('UPDATE `projects` set tenant_id=company_id;'); 
-        
-        $this->comment('Copying companies to tenants');
-        DB::statement('TRUNCATE TABLE `tenants`');
-        DB::statement('INSERT INTO tenants (id, name) select c.id, c.company_name from companies c;'); 
-
         $this->comment('Making some users table column nullable');
+
         DB::statement('ALTER TABLE `users` CHANGE `usertype` `usertype` int NULL;');
         DB::statement('ALTER TABLE `users` CHANGE `user_roles` `user_roles` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL');
         DB::statement('ALTER TABLE `users` CHANGE `address` `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;');
@@ -68,6 +74,8 @@ class CoreappMigrate extends Command
         DB::statement('ALTER TABLE `users` CHANGE `name` `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;');
 
         $this->comment('All Done!');
+        
+        $this->comment('If you need Coreapp data locally, import all table data without migrations table data and start import after setting "SET foreign_key_checks = 0;" first.');
         
         return 0;
     }
